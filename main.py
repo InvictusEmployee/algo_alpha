883478b0872ab9565badbfbe591a856a8a4913a0
Yamamoto <61530000+InvictusEmployee@users.noreply.github.com>
2022-08-29 21:08:37 +0700

    doc: limitation of the code

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
-input = ['ab', 'bc', 'cd', 'de', 'ef']
-input2 = ['ab', 'bc', 'ab', 'de', 'ef']
-target = 'abab'
-outputEx = ['ab', 'cd']
-output = []
+input1 = ['ab', 'bc', 'cd']
+input4 = ['ab', 'ba', 'ab']
+target1 = 'abcd'
+target2  = 'cdab'
+target3 = 'abab'
+target4 = 'abab'
 
+# limitation
+# this function can not find if the 2 words in the array can be a permutated to the target word.
+# this function can only return 2 combined words (or None) in the array, not more or less.
+# this function doesn't check if the words in the  array has a duplicate one.
 def process(input: list, target: str):
   output = []
   for i in input:
-    # word = input[i]
     if target.startswith(i):
       input.remove(i)
       output.append(i)
       target = target[len(i):]
-      # print('i', i)
-      # print('input', input)
-      # print('output', output)
-      # print('target', target)
       for e in input:
         if target.startswith(e):
           output.append(e)
-          # print('e', e)
           return output
   return None
 
 
-print(process(input, target))
+print(process(input4, target4))
