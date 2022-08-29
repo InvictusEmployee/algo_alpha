edd7c1889cde138902f0f9b1d0424c89b3d49e71
Yamamoto <61530000+InvictusEmployee@users.noreply.github.com>
2022-08-29 17:45:02 +0700

    refactor

--- main.py
+++ main.py
@@ -4,21 +4,6 @@ target = 'abab'
 outputEx = ['ab', 'cd']
 output = []
 
-
-# def process(input: list, target: str, output: list):
-#   # output = []
-#   currentTarget = target
-#   for i in input:
-#     # word = input[i]
-#     if target.startswith(i):
-#       output.append(i)
-#       input.remove(i)
-#       currentTarget = currentTarget[len(i):]
-#       return process(input, currentTarget, output)
-#       # if i[0] == target[0] and i[-1] == target[-1]:
-#       #     output.append(i)
-#   return output
-
 def process(input: list, target: str):
   output = []
   for i in input:
